08f2129be9e0960c765a9bdbb9157958f1de8c05
Camilo Andres Garatejo Moreno <cgaratej@car14s3.42barcelona.com>
2024-06-06 15:01:36 +0200

    add

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,22 +6,22 @@
 /*   By: cgaratej <cgaratej@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 15:04:48 by cgaratej          #+#    #+#             */
-/*   Updated: 2024/06/05 18:19:31 by cgaratej         ###   ########.fr       */
+/*   Updated: 2024/06/06 15:01:11 by cgaratej         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdlib.h>
 
-#ifndef BUFERR_SIZE
-# define BUFERR_SIZE 42
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
 #endif
 
 int	ft_strlen(char *str)
 {
 	int	i = 0;
 
-	if (str[i])
+	while (str[i])
 		i++;
 	return (i);
 }
@@ -33,13 +33,13 @@ char	*ft_strchr(char *str, char c)
 	while (str[i])
 	{
 		if (str[i] == c)
-			return (str[i]);
+			return ((char *)&str[i]);
 		i++;
 	}
 	return (NULL);
 }
 
-char	ft_strcpy(char *dest, char *org)
+char	*ft_strcpy(char *dest, char *org)
 {
 	int	i = 0;
 	
@@ -49,11 +49,13 @@ char	ft_strcpy(char *dest, char *org)
 		i++;
 	}
 	dest[i] = '\0';
+	return (dest);
 }
 
 char	*ft_strdup(char *str)
 {
-	char	*cpy = malloc(ft_strlen(str) + 1);
+	int 	len = ft_strlen(str);
+	char	*cpy = malloc(len + 1);
 	
 	if (!cpy)
 		return (NULL);
@@ -65,32 +67,61 @@ char *ft_strjoin(char *s1, char *s2)
 {
 	int		len1 = ft_strlen(s1);
 	int		len2 = ft_strlen(s2);
-	char	*str = malloc(len1 + len2 + 1);
-	int		i = 0;
-	int		j = 0;
-	
-	while (s1[i])
-	{
-		str[j] = s1[i];
-		i++;
-		j++;
-	}
-	i = 0;
-	while (s2[i])
-	{
-		str[j] = s2[i];
-		i++;
-		j++;
-	}
+	char	*join = malloc(len1 + len2 + 1);
+
+	ft_strcpy(join, s1);
+	ft_strcpy(join + len1, s2);
 	free(s1);
-	return (str);
+	return (join);
 }
 
-char	*get_next_line(int fd)
+char *get_next_line(int fd)
 {
-	static char buffer[BUFERR_SIZE + 1];
+	static char buf[BUFFER_SIZE + 1];
 	char *line;
-	char *new_line;
-	int bitys;
+	char *newline;
+	int bytes;
 	int to_copy;
+
+	line = ft_strdup(buf);
+	while (!(newline = ft_strchr(line, '\n')) && (bytes = read(fd, buf, BUFFER_SIZE)))
+	{
+		buf[bytes] = '\0';
+		line = ft_strjoin(line, buf);
+	}
+	if (!ft_strlen(line))
+		return (free(line), NULL);
+	if (newline)
+	{
+		to_copy = newline - line + 1;
+		ft_strcpy(buf, newline + 1);
+	}
+	else
+	{
+		to_copy = ft_strlen(line);
+		buf[0] = '\0';
+	}
+	line[to_copy] = '\0';
+	return (line);
+}
+
+#include <stdio.h>
+#include <fcntl.h>
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+	int		fd;
+	char	*line;
+
+	fd = open(argv[1], O_RDONLY);
+	while (1)
+	{
+		line = get_next_line(fd);
+		if (line == NULL)
+			break ;
+		printf("%s", line);
+		free(line);
+	}
+	return (0);
 }
